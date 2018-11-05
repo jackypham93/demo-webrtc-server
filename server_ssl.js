d3a69b2ff79590f52a0a248608a9ca4447eb5c63
jackypham93 <phamphunghao93@gmail.com>
2018-11-05 21:38:18 +0700

    Fix bug

--- server_ssl.js
+++ server_ssl.js
@@ -8,7 +8,7 @@ var io      = require("socket.io"); // web socket external module
 // To use this server_example folder only without parent folder:
 // 1. you need to replace this "require("../");" by "require("easyrtc");"
 // 2. install easyrtc (npm i easyrtc --save) in server_example/package.json
-var easyrtc = require("..");   // EasyRTC internal module
+var easyrtc = require("easyrtc");   // EasyRTC internal module
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
