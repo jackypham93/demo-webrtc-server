67a40517e8800abec5716d809bde3223fda3563b
jackypham93 <phamphunghao93@gmail.com>
2018-11-05 23:53:52 +0700

    The seccond commit.

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-// Load required modules
+﻿// Load required modules
 var http = require("http");              // http server core module
 var express = require("express");           // web framework external module
 var serveStatic = require('serve-static');  // serve static files
 var socketIo = require("socket.io");        // web socket external module
-
-// This sample is using the easyrtc from parent folder.
-// To use this server_example folder only without parent folder:
-// 1. you need to replace this "require("../");" by "require("easyrtc");"
-// 2. install easyrtc (npm i easyrtc --save) in server_example/package.json
-
-var easyrtc = require("easyrtc"); // EasyRTC internal module
+var easyrtc = require("easyrtc");               // EasyRTC external module
 
 // Set process name
 process.title = "node-easyrtc";
@@ -19,7 +13,7 @@ var app = express();
 app.use(serveStatic('static', { 'index': ['index.html'] }));
 
 // Start Express http server on port 8080
-var webServer = http.createServer(app);
+var webServer = http.createServer(app).listen(9090);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = socketIo.listen(webServer, { "log level": 1 });
@@ -47,7 +41,7 @@ easyrtc.events.on("roomJoin", function (connectionObj, roomName, roomParameter, 
     console.log("[" + connectionObj.getEasyrtcid() + "] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
     easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
-
+easyrtc.setOption('appIceServers', [{ url: 'stun:stun.l.google.com:19302' }], 'roomDefaultEnable', true);
 // Start EasyRTC server
 var rtc = easyrtc.listen(app, socketServer, null, function (err, rtcRef) {
     console.log("Initiated");
@@ -61,5 +55,5 @@ var rtc = easyrtc.listen(app, socketServer, null, function (err, rtcRef) {
 
 //listen on port 8080
 webServer.listen(9090, function () {
-    console.log('listening on http://localhost:9090');
+    console.log('listening on http://localhost:8080');
 });
